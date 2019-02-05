b8255e441ebe89f12689c4346a1e197441fda0e8
Paul Moore <pdmoore@gmail.com>
2019-02-04 20:16:36 -0800

    solution to Square Every Digit

--- src/SquareDigit.java
+++ src/SquareDigit.java
@@ -1,5 +1,13 @@
 public class SquareDigit {
-    public int squareDigits(int i) {
-        return 0;
+    public int squareDigits(int input) {
+        char[] digits = Integer.toString(input).toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char digit :
+                digits) {
+            int thisDigit = Character.getNumericValue(digit);
+            sb.append(thisDigit * thisDigit);
+        }
+
+        return Integer.parseInt(sb.toString());
     }
 }
--- test/SquareDigitTest.java
+++ test/SquareDigitTest.java
@@ -3,8 +3,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareDigitTest {
+
     @Test
-    public void test() {
+    public void singleDigit() {
+        assertEquals(16, new SquareDigit().squareDigits(4));
+    }
+
+    @Test
+    public void test_MultipleDigits() {
         assertEquals(811181, new SquareDigit().squareDigits(9119));
     }
 
